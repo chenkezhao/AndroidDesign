6fe5979aa6cc80d377af5d18f1fe96a9087db538
Aaron <310771881@qq.com>
2016-05-26 11:44:21 +0800

    SlidingPaneLayout简单使用
    http://www.jianshu.com/p/67ce59c9e747

--- app/src/main/java/zhao/androiddesign/activity/MainActivity.java
+++ app/src/main/java/zhao/androiddesign/activity/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView mListView;
     private String[] menus = {"AppBarLayout、TabLayout、ViewPager", "CollapsingToolbarLayout", "BottomSheetBehavior、" +
             "BottomSheetDialog(代替Popupwindow)、FloatingActionButton", "Snackbar",
-            "SwipeDismissBehavior", "TabLayout TabItem、TextInputLayout","自定义 ActionBar","ActionBar的升级版ToolBar"};
+            "SwipeDismissBehavior", "TabLayout TabItem、TextInputLayout","自定义 ActionBar","ActionBar的升级版ToolBar","SlidingPaneLayout"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +83,9 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 7:
                         intent = new Intent(MainActivity.this, ToolbarActivity.class);
+                        break;
+                    case 8:
+                        intent = new Intent(MainActivity.this, SlidingPaneLayoutActivity.class);
                         break;
                     default:
                         flag = false;
---
+++ app/src/main/java/zhao/androiddesign/activity/SlidingPaneLayoutActivity.java
@@ -0,0 +1,15 @@
+package zhao.androiddesign.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zhao.androiddesign.R;
+
+public class SlidingPaneLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_pane_layout);
+    }
+}
