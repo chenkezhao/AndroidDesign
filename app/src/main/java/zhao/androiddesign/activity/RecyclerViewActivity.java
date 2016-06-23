362f8e143db5b807f3b12f8fbe5c5e8e9599965b
Aaron <310771881@qq.com>
2016-06-23 10:10:12 +0800

    RecyclerView适配器加载View时的动画(存在已加载的View，动画失效)。另外Spinner选择面板及enum枚举类型使用
    使用的第三方库，可以继承，然后实现自己定制的RecyclerView动画
    github-https://github.com/wasabeef/recyclerview-animators
    PS:还有RecyclerView的Item增删的动画效果

--- app/src/main/java/zhao/androiddesign/activity/RecyclerViewActivity.java
+++ app/src/main/java/zhao/androiddesign/activity/RecyclerViewActivity.java
@@ -1,33 +1,82 @@
 package zhao.androiddesign.activity;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.view.menu.MenuItemImpl;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.AnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInBottomAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInLeftAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInRightAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.LandingAnimator;
 import zhao.androiddesign.R;
 import zhao.androiddesign.adapter.RecyclerAdapter;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private Spinner spinner;
+
+    /**
+     * 枚举类型
+     */
+    private enum Type {
+        AlphaIn {
+            @Override public AnimationAdapter get(Context context) {
+                RecyclerAdapter adapter = new RecyclerAdapter(context);
+                return new AlphaInAnimationAdapter(adapter);
+            }
+        },
+        ScaleIn {
+            @Override public AnimationAdapter get(Context context) {
+                RecyclerAdapter adapter = new RecyclerAdapter(context);
+                return new ScaleInAnimationAdapter(adapter);
+            }
+        },
+        SlideInBottom {
+            @Override public AnimationAdapter get(Context context) {
+                RecyclerAdapter adapter = new RecyclerAdapter(context);
+                return new SlideInBottomAnimationAdapter(adapter);
+            }
+        },
+        SlideInLeft {
+            @Override public AnimationAdapter get(Context context) {
+                RecyclerAdapter adapter = new RecyclerAdapter(context);
+                return new SlideInLeftAnimationAdapter(adapter);
+            }
+        },
+        SlideInRight {
+            @Override public AnimationAdapter get(Context context) {
+                RecyclerAdapter adapter = new RecyclerAdapter(context);
+                return new SlideInRightAnimationAdapter(adapter);
+            }
+        };
+
+        public abstract AnimationAdapter get(Context context);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         initView();
     }
 
@@ -37,6 +86,31 @@ public class RecyclerViewActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));//线性布局，类似ListView
         //适配器
         recyclerView.setAdapter(new RecyclerAdapter(this));
+
+
+
+        //选择面板
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        for(Type type:Type.values()){
+            spinnerAdapter.add(type.name());
+        }
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                AnimationAdapter adapter = Type.values()[position].get(RecyclerViewActivity.this);
+                adapter.setFirstOnly(true);
+                adapter.setDuration(500);
+                adapter.setInterpolator(new OvershootInterpolator(0.5f));
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
